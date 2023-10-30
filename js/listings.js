15f902d026bf3e75a6e547a0c4141834a27c4789
justbraf <dwaynecorey@gmail.com>
2023-10-30 15:42:29 -0400

    listing feature

--- js/listings.js
+++ js/listings.js
@@ -20,13 +20,47 @@ let getData = () => {
         })
         .then((data) => {
             let teamDat = document.querySelector(".teamData")
-            teamDat.innerHTML = "<h4>" + data["name"] + "</h4>"
+            // teamDat.innerHTML = "<h4>" + data["name"] + "</h4>"
             data["clubs"].forEach(club => {
-                teamDat.innerHTML += "<p>" + club["name"] + "</p>"
-                
-            });
+                let bkCol = `#e2b7${Math.random().toString(16).substring(2, 4)}`
+                teamDat.innerHTML += `
+                 <div class="col-xl-3 text-center mt-3">
+                    <div class="card" style="width: 18rem; background: ${bkCol};">
+                        <div class="card-body">
+                            <h5 class="card-title">${club.name}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">${club.code}</h6>
+                            <p class="card-text">This team plays for ${club.country}.</p>
+                            <a class="btn btn-info" data-name="${club.name}" data-code="${club.code}" onclick="addToFav(event)" >Add to Favourites</a>
+                        </div>
+                    </div>
+                </div>
+                `
+                // a = a + sum
+                // a += sum
+            })
         })
-        .catch(()=>{
+        .catch(() => {
             console.error("cry for help")
         })
+}
+
+let addToFav = (event) => {
+    let currentUser = JSON.parse(sessionStorage.getItem("currentUser"))
+    let userData = JSON.parse(localStorage.getItem(currentUser.username))
+    if (!userData.favourites)
+        userData.favourites = []
+    let team = {
+        code: event.target.getAttribute("data-code"),
+        name: event.target.getAttribute("data-name")
+    }
+    let found = false
+    if (userData.favourites.length > 0)
+        userData.favourites.forEach(fav => {
+            if (fav.code == team.code)
+                found = true
+        })
+    if (!found) {
+        userData.favourites.push(team)
+        localStorage.setItem(currentUser.username, JSON.stringify(userData))
+    }
 }
